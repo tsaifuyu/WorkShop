fccf4b076bc371b0752efb593f93ef9ca496301a
tsaifuyu <imsuperfuyu@gmail.com>
2023-04-22 18:58:52 -0700

    v1

---
+++ src/views/Products.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="text-end">
+    <button class="btn btn-primary" type="button" @click="openModal">
+      增加一個商品
+    </button>
+  </div>
+  <table class="table mt-4">
+    <thead>
+      <tr>
+        <th width="120">分類</th>
+        <th>產品名稱</th>
+        <th width="120">原價</th>
+        <th width="120">售價</th>
+        <th width="100">是否啟用</th>
+        <th width="200">編輯</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in products" :key="item.id">
+        <td>{{ item.category }}</td>
+        <td>{{ item.title }}</td>
+        <td class="text-right">
+          {{ item.origin_price }}
+        </td>
+        <td class="text-right">
+          {{ item.price }}
+        </td>
+        <td>
+          <span class="text-success" v-if="item.is_enabled">啟用</span>
+          <span class="text-muted" v-else>未啟用</span>
+        </td>
+        <td>
+          <div class="btn-group">
+            <button class="btn btn-outline-primary btn-sm">編輯</button>
+            <button class="btn btn-outline-danger btn-sm">刪除</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <ProductModal ref="productModal" :product="tempProduct" @update-product="updateProduct"></ProductModal>
+</template>
+
+<script>
+import ProductModal from '../components/ProductModal.vue'
+export default {
+  data () {
+    return {
+      products: [],
+      pagination: {},
+      tempProduct: {}
+    }
+  },
+  // 區域註冊
+  components: {
+    ProductModal
+  },
+  methods: {
+    getProducts () {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          console.log(res.data)
+          this.products = res.data.products
+          this.pagination = res.data.pagination
+        }
+      })
+    },
+    openModal () {
+      this.tempProduct = {}
+      const productComponent = this.$refs.productModal
+      productComponent.showModal()
+    },
+    updateProduct (item) {
+      this.tempProduct = item
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
+      const productComponent = this.$refs.productModal
+      this.$http.post(api, { data: this.tempProduct }).then((reponse) => {
+        console.log(reponse)
+        productComponent.hideModal()
+        this.getProducts()
+      }
+      )
+    }
+  },
+  created () {
+    this.getProducts()
+  }
+}
+</script>
